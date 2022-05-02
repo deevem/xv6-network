a3da501b0fb19124908d824a2849294482d5f4f0
adfwerUbuntu <ba123698745@126.com>
2022-05-02 16:59:29 +0800

    add recv arp test

--- net/arptest.c
+++ net/arptest.c
@@ -3,16 +3,21 @@
 
 int main() {
     tap_init();
+    sleep(2);
+    int headroom = sizeof(struct eth_hdr) + sizeof(struct arp_hdr);
+    struct mbuf* m = mbufalloc(headroom);
+    arp_tx(ARP_OP_REQUEST, broadcast_mac, MAKE_IP_ADDR(101, 5, 172, 230));
+
     int t = 0;
     while(1) {
-        sleep(2);
         t++;
-        printf("%s %d\n", "ARP test ", t);
+        struct mbuf* m = mbufalloc(0);
+        int n = tun_read(m->buf, MBUF_SIZE);
 
-        int headroom = sizeof(struct eth_hdr) + sizeof(struct arp_hdr);
-
-        struct mbuf* m = mbufalloc(headroom);
-        arp_tx(ARP_OP_REQUEST, broadcast_mac, MAKE_IP_ADDR(101, 5, 172, 230));
+        printf("%s %d %d\n", "tun finished", t, n);
+        m->len = n;
+        eth_rx(m);
     }
+
     return 0;
 }
