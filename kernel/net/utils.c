fec294d4ec359d0a55665ea06208604b2a8b0f03
deevem <deevem@126.com>
2022-06-19 22:09:51 +0800

    tcp_checksum

--- kernel/net/utils.c
+++ kernel/net/utils.c
@@ -27,12 +27,34 @@ uint16_t checksum(const unsigned char* addr, int len) {
 
 }
 
-uint32 tcp_checksum(struct mbuf* m, uint32_t saddr, uint32_t daddr){
-    uint32 sum = 0;
+uint16_t tcp_checksum(struct mbuf* m, uint32_t saddr, uint32_t daddr){
+    int len = m->len;
+    
+
+    int nleft = len;
+    const uint16_t *w = (const uint16_t*) m;
+    uint32_t sum = 0;
+    uint16_t answer = 0;
+
     sum += saddr;
     sum += daddr;
     sum += htons(IPPROTO_TCP);
     sum += htons(m->len);
-    sum += checksum(m->head,m->len);
-    return sum;
+
+    while(nleft > 1) {
+        sum += *w++;
+        nleft -= 2;
+    }
+
+    if (nleft == 1) {
+        *(unsigned char *)(&answer) = *(const unsigned char *)w;
+        sum += answer;
+    }
+
+    sum = (sum & 0xffff) + (sum >> 16);
+    sum += (sum >> 16);
+
+    answer = ~sum; 
+
+    return answer;
 }
